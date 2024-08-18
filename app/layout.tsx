987c4639d54de1f4c3580391545275a914a761b5
mina-gwak <mina.gwak@kakao.com>
2024-08-18 22:08:32 +0900

    Chore: PD-18 App 디렉토리를 루트로 이동
    - FSD 아키텍처를 사용해보기로 결정함
    - https://feature-sliced.design/

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,23 +7,23 @@ import './globals.css'
 const spoqaHanSansNeo = localFont({
   src: [
     {
-      path: '../../public/fonts/SpoqaHanSansNeo-Thin.woff2',
+      path: '../public/fonts/SpoqaHanSansNeo-Thin.woff2',
       weight: '100',
     },
     {
-      path: '../../public/fonts/SpoqaHanSansNeo-Light.woff2',
+      path: '../public/fonts/SpoqaHanSansNeo-Light.woff2',
       weight: '300',
     },
     {
-      path: '../../public/fonts/SpoqaHanSansNeo-Regular.woff2',
+      path: '../public/fonts/SpoqaHanSansNeo-Regular.woff2',
       weight: '400',
     },
     {
-      path: '../../public/fonts/SpoqaHanSansNeo-Medium.woff2',
+      path: '../public/fonts/SpoqaHanSansNeo-Medium.woff2',
       weight: '500',
     },
     {
-      path: '../../public/fonts/SpoqaHanSansNeo-Bold.woff2',
+      path: '../public/fonts/SpoqaHanSansNeo-Bold.woff2',
       weight: '700',
     },
   ],
---
+++ pages/README.md
@@ -0,0 +1,4 @@
+This folder is added to resolve a conflict with the NextJS pages folder.
+
+https://feature-sliced.design/docs/guides/tech/with-nextjs#app-router
+```
