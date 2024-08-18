efe93ea29bc47c24ba678898042fdb22e343da54
mina-gwak <mina.gwak@kakao.com>
2024-08-19 00:58:27 +0900

    Refactor: PD-18 variable 생성 후 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,9 @@
 import type { Metadata } from 'next'
-import localFont from 'next/font/local'
 import { ReactNode } from 'react'
 
-import './globals.css'
+import { SpoqaHanSansNeo } from 'app/styles'
 
-const spoqaHanSansNeo = localFont({
-  src: [
-    {
-      path: '../public/fonts/SpoqaHanSansNeo-Thin.woff2',
-      weight: '100',
-    },
-    {
-      path: '../public/fonts/SpoqaHanSansNeo-Light.woff2',
-      weight: '300',
-    },
-    {
-      path: '../public/fonts/SpoqaHanSansNeo-Regular.woff2',
-      weight: '400',
-    },
-    {
-      path: '../public/fonts/SpoqaHanSansNeo-Medium.woff2',
-      weight: '500',
-    },
-    {
-      path: '../public/fonts/SpoqaHanSansNeo-Bold.woff2',
-      weight: '700',
-    },
-  ],
-  display: 'swap',
-})
+import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Plus82',
@@ -41,8 +16,8 @@ export default function RootLayout({
   children: ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={spoqaHanSansNeo.className}>{children}</body>
+    <html lang="en" className={SpoqaHanSansNeo.variable}>
+      <body className="font-spoqa-han-sans-neo">{children}</body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'spoqa-han-sans-neo': ['var(--font-spoqa-han-sans-neo)'],
+      },
+    },
   },
   plugins: [],
 }
