b2d1b5699e92054b6959fe3a8c64803cca796609
mina-gwak <mina.gwak@kakao.com>
2024-09-15 18:11:22 +0900

    Refactor: PD-49 스타일 토큰을 tailwind 설정에서 따로 분리

---
+++ src/shared/config/index.ts
@@ -0,0 +1,1 @@
+export { colors, fontSize } from './tokens'
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+import { colors, fontSize } from './src/shared/config'
+
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
@@ -7,44 +9,8 @@ const config: Config = {
       fontFamily: {
         'spoqa-han-sans-neo': ['var(--font-spoqa-han-sans-neo)'],
       },
-      colors: {
-        white: '#FFFFFF',
-        black: '#000000',
-        error: '#E41212',
-        success: '#00CB3D',
-        gray: {
-          100: '#F5F5F5',
-          200: '#EEEEEE',
-          300: '#E0E0E0',
-          400: '#BDBDBD',
-          500: '#9E9E9E',
-          600: '#757575',
-          700: '#616161',
-          800: '#424242',
-          900: '#212121',
-        },
-        blue: {
-          50: '#F0F4FF',
-          100: '#D9E8FF',
-          200: '#92CFFF',
-          300: '#64BBFF',
-          400: '#41ABFF',
-          500: '#269CFF',
-          600: '#2D8DFF',
-          700: '#2F7AF3',
-          800: '#3068E0',
-          900: '#0054D1',
-        },
-      },
-      fontSize: {
-        xs: '0.75rem', // 12
-        sm: '0.875rem', // 14
-        base: '1rem', // 16
-        lg: '1.125rem', // 18
-        xl: '1.5rem', // 24
-        '2xl': '1.75rem', // 28
-        '3xl': '2rem', // 32
-      },
+      colors,
+      fontSize,
     },
   },
   plugins: [],
