a76219c7cdc1c26d00034d2672d2ee9295740176
mina-gwak <mina.gwak@kakao.com>
2024-09-20 18:29:54 +0900

    Feat: PD-45 Radio 컴포넌트, useRadio 훅 추가

---
+++ src/shared/lib/radio.ts
@@ -0,0 +1,15 @@
+import { useCallback, useState } from 'react'
+
+export type CheckedValue = string | number | undefined
+
+export const useRadio = () => {
+  const [checkedValue, setCheckedValue] = useState<CheckedValue>()
+
+  const updateCheckedValue = useCallback((value: CheckedValue) => {
+    setCheckedValue(value)
+  }, [])
+
+  const isChecked = (value: CheckedValue) => checkedValue === value
+
+  return { checkedValue, isChecked, updateCheckedValue }
+}
