93a615001ee0522f72306b86fd3164d748ab7e01
mina-gwak <mina.gwak@kakao.com>
2024-09-22 01:16:33 +0900

    Feat: PD-44 useCheckbox 훅 추가

---
+++ src/shared/lib/checkbox.ts
@@ -0,0 +1,118 @@
+import { isEqual } from 'lodash-es'
+import { useEffect, useState } from 'react'
+
+import { CheckboxValue } from 'shared/ui'
+
+import { List } from './list'
+
+export type CheckboxChangeHandler = (
+  callback?: (updatedCheckedValues: CheckboxValue[]) => void,
+) => void
+
+export type GetCheckboxPropsReturnType = {
+  name?: string
+  value: CheckboxValue
+  checked: boolean
+  onChange: CheckboxChangeHandler
+}
+
+export type GetIndeterminateCheckboxPropsReturnType = Omit<
+  GetCheckboxPropsReturnType,
+  'value'
+> & {
+  value?: CheckboxValue
+  indeterminate: boolean | null
+}
+
+export type UseCheckboxProps = {
+  name?: string
+  options: CheckboxValue[]
+  defaultCheckedValues?: CheckboxValue[]
+}
+
+export const useCheckbox = ({
+  name,
+  options,
+  defaultCheckedValues,
+}: UseCheckboxProps) => {
+  const [checkedValues, setCheckedValues] = useState<CheckboxValue[]>(
+    defaultCheckedValues ?? [],
+  )
+
+  useEffect(() => {
+    if (defaultCheckedValues && !isEqual(defaultCheckedValues, checkedValues)) {
+      setCheckedValues(defaultCheckedValues)
+    }
+  }, [defaultCheckedValues])
+
+  const isChecked = (value: CheckboxValue) => checkedValues.includes(value)
+
+  const isAllChecked = () => List(checkedValues).equal(options)
+
+  const isIndeterminate = () => {
+    if (List(checkedValues).isEmpty()) return null
+
+    return !isAllChecked()
+  }
+
+  const toggleChecked =
+    (value: CheckboxValue) =>
+    (prevCheckedValues = checkedValues) => {
+      if (isChecked(value)) return List(prevCheckedValues).remove(value).get()
+
+      return List(prevCheckedValues).add(value).get()
+    }
+
+  const toggleAllChecked = () => (isAllChecked() ? [] : options)
+
+  const updateCheckedValue = (value: CheckboxValue) => {
+    setCheckedValues(toggleChecked(value))
+  }
+
+  const resetCheckedValues = () => {
+    setCheckedValues([])
+  }
+
+  const handleCheckboxChange =
+    (value: CheckboxValue): CheckboxChangeHandler =>
+    callback => {
+      const updatedCheckedValues = toggleChecked(value)()
+      setCheckedValues(updatedCheckedValues)
+      callback?.(updatedCheckedValues)
+    }
+
+  const handleIndeterminateCheckboxChange: CheckboxChangeHandler = callback => {
+    const updatedCheckedValues = toggleAllChecked()
+    setCheckedValues(updatedCheckedValues)
+    callback?.(updatedCheckedValues)
+  }
+
+  const getCheckboxProps = (
+    value: CheckboxValue,
+  ): GetCheckboxPropsReturnType => ({
+    name,
+    value,
+    checked: isChecked(value),
+    onChange: handleCheckboxChange(value),
+  })
+
+  const getIndeterminateCheckboxProps = (
+    value?: CheckboxValue,
+  ): GetIndeterminateCheckboxPropsReturnType => ({
+    name,
+    value,
+    checked: !List(checkedValues).isEmpty(),
+    onChange: handleIndeterminateCheckboxChange,
+    indeterminate: isIndeterminate(),
+  })
+
+  return {
+    checkedValues,
+    isChecked,
+    isAllChecked,
+    updateCheckedValue,
+    resetCheckedValues,
+    getCheckboxProps,
+    getIndeterminateCheckboxProps,
+  }
+}
