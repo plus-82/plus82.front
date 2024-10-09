38e42baba04e27fe8e210fc753338b0f0794acfd
mina-gwak <mina.gwak@kakao.com>
2024-10-09 14:51:09 +0900

    Chore: PD-69 CORS 에러 해결 위한 proxy 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,14 @@ const nextConfig = {
     });
     return config;
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `https://back.plus82.co/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
