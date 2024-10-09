8f1e2e369d6b741a9f9b20062ffdcbd8287822cf
mina-gwak <mina.gwak@kakao.com>
2024-10-09 17:41:54 +0900

    Feat: PD-69 SignIn 기능 추가

--- src/pages/sign-in/ui/sign-in-page/index.tsx
+++ src/pages/sign-in/ui/sign-in-page/index.tsx
@@ -1,19 +1,60 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 
+import { AuthExceptionCode, HttpError } from 'shared/api'
 import { hasError } from 'shared/lib'
-import { Button, Label, Layout, Link, TextField } from 'shared/ui'
+import { Button, HelperText, Label, Layout, Link, TextField } from 'shared/ui'
+
+import { useSignIn } from '../../api/use-sign-in'
 
 import * as css from './variants'
 
-interface SignInFieldValues {
+interface FormValues {
   email: string
   password: string
 }
 
+const defaultValues: FormValues = {
+  email: '',
+  password: '',
+}
+
 export const SignInPage = () => {
-  const { register, getFieldState } = useForm<SignInFieldValues>()
+  const router = useRouter()
+
+  const { register, handleSubmit, getFieldState, setError } =
+    useForm<FormValues>({
+      defaultValues,
+      reValidateMode: 'onSubmit',
+    })
+
+  const signIn = useSignIn()
+
+  const handleSignInSuccess = () => {
+    router.replace('/')
+  }
+
+  const handleSignInError = (error: HttpError) => {
+    if (error.code === AuthExceptionCode.EMAIL_NOT_CORRECT) {
+      setError('email', {
+        message: 'You do not have a history of signing up for this email',
+      })
+    } else if (error.code === AuthExceptionCode.PW_NOT_CORRECT) {
+      setError('password', {
+        message:
+          'The password does not appear to be correct. Please check again.',
+      })
+    }
+  }
+
+  const handleFormValid = (data: FormValues) => {
+    signIn.mutate(data, {
+      onSuccess: handleSignInSuccess,
+      onError: handleSignInError,
+    })
+  }
 
   return (
     <Layout className={css.layout()}>
@@ -24,22 +65,34 @@ export const SignInPage = () => {
             <Label>Email</Label>
             <TextField
               {...register('email', { required: true })}
-              placeholder="Please enter your email"
               error={hasError(getFieldState('email').error)}
             />
+            {hasError(getFieldState('email').error) && (
+              <HelperText variant="error">
+                {getFieldState('email').error?.message}
+              </HelperText>
+            )}
           </div>
           <div className={css.field()}>
             <Label>Password</Label>
             <TextField
               {...register('password', { required: true })}
               type="password"
-              placeholder="Please enter your password"
               error={hasError(getFieldState('password').error)}
             />
+            {hasError(getFieldState('password').error) && (
+              <HelperText variant="error">
+                {getFieldState('password').error?.message}
+              </HelperText>
+            )}
           </div>
         </div>
         <div className={css.buttonGroup()}>
-          <Button size="large" fullWidth>
+          <Button
+            size="large"
+            fullWidth
+            onClick={handleSubmit(handleFormValid)}
+          >
             Sign In
           </Button>
           <Link href="/" variant="tertiary">
