1d9f68783adeb71e0fe8b8b062c673345663d2db
mina-gwak <mina.gwak@kakao.com>
2024-10-18 00:07:37 +0900

    Fix: PD-91 PasswordField에서 ref를 전달하도록 수정
    - ref를 전달하지 않을 경우 register() 형식으로 전달할 때 값이 제대로 업데이트되지 않음

--- src/shared/ui/text-field/password-field.tsx
+++ src/shared/ui/text-field/password-field.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 
 import { colors } from 'shared/config'
 import { Slot } from 'shared/lib'
@@ -13,30 +13,38 @@ interface Props extends TextFieldProps {
   showToggle?: boolean
 }
 
-export const PasswordField = ({ showToggle, ...restProps }: Props) => {
-  const [showPassword, setShowPassword] = useState(false)
+export const PasswordField = forwardRef<HTMLInputElement, Props>(
+  ({ showToggle, ...restProps }, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(prev => !prev)
-  }
+    const togglePasswordVisibility = () => {
+      setShowPassword(prev => !prev)
+    }
 
-  return (
-    <TextField {...restProps} type={showPassword ? 'text' : 'password'}>
-      {showToggle && (
-        <Slot name="right">
-          <button
-            type="button"
-            aria-label="Toggle password visibility"
-            onClick={togglePasswordVisibility}
-          >
-            <Icon
-              name={showPassword ? 'EyesOn' : 'EyesOff'}
-              size="large"
-              color={colors.gray['500']}
-            />
-          </button>
-        </Slot>
-      )}
-    </TextField>
-  )
-}
+    return (
+      <TextField
+        {...restProps}
+        type={showPassword ? 'text' : 'password'}
+        ref={ref}
+      >
+        {showToggle && (
+          <Slot name="right">
+            <button
+              type="button"
+              aria-label="Toggle password visibility"
+              onClick={togglePasswordVisibility}
+            >
+              <Icon
+                name={showPassword ? 'EyesOn' : 'EyesOff'}
+                size="large"
+                color={colors.gray['500']}
+              />
+            </button>
+          </Slot>
+        )}
+      </TextField>
+    )
+  },
+)
+
+PasswordField.displayName = 'PasswordField'
