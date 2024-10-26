08feb8d9955e05ed4fdb9e6cf8e59d42d8bd46c5
mina-gwak <mina.gwak@kakao.com>
2024-10-27 01:36:58 +0900

    Feat: PD-91 버튼 컴포넌트가 링크처럼 동작하게 되는 경우 as prop을 통해 a 태그로 렌더링

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -1,21 +1,39 @@
-import { ButtonHTMLAttributes } from 'react'
+import Link from 'next/link'
+import { ComponentProps, ElementType } from 'react'
 
 import { cn } from 'shared/lib'
 import { button, ButtonVariants } from 'shared/ui/button/variants'
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & ButtonVariants
+type ButtonProps = ButtonVariants & {
+  as?: 'button' | 'a'
+}
 
-export const Button = ({
-  type = 'button',
+type Props<E extends ElementType> = ButtonProps &
+  Omit<ComponentProps<E>, keyof ButtonProps>
+
+export const Button = <E extends ElementType>({
+  as,
   variant = 'primary',
   size = 'medium',
   fullWidth = false,
   className,
   ...restProps
-}: Props) => {
+}: Props<E>) => {
+  const $Element = as || 'button'
+
+  if (as === 'a') {
+    return (
+      <Link href={restProps?.href} passHref legacyBehavior>
+        <$Element
+          className={cn(button({ variant, size, fullWidth, className }))}
+          {...restProps}
+        />
+      </Link>
+    )
+  }
+
   return (
-    <button
-      type={type}
+    <$Element
       className={cn(button({ variant, size, fullWidth, className }))}
       {...restProps}
     />
