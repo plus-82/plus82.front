d818085eaff03e3c68f8053204ea57ba531dac67
mina-gwak <mina.gwak@kakao.com>
2024-10-28 23:52:00 +0900

    Feat: PD-111 Find password 페이지 마크업

--- src/pages/password/ui/find-password/index.tsx
+++ src/pages/password/ui/find-password/index.tsx
@@ -1,5 +1,31 @@
 'use client'
 
+import { useForm } from 'react-hook-form'
+
+import { Button, Label, Layout } from 'shared/ui'
+
+import { Form } from 'features/form'
+
+import * as css from './variants'
+
 export const FindPasswordPage = () => {
-  return <div>Find password page</div>
+  const form = useForm()
+
+  return (
+    <Layout className={css.layout()}>
+      <h1 className={css.heading()}>Reset password</h1>
+      <Form {...form} className={css.form()}>
+        <div className={css.field()}>
+          <Label>Email</Label>
+          <Form.Control name="email">
+            <Form.TextField />
+            <Form.ErrorMessage />
+          </Form.Control>
+        </div>
+        <Button size="large" fullWidth>
+          Send password reset link
+        </Button>
+      </Form>
+    </Layout>
+  )
 }
