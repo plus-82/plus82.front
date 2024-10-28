d26c70ba47865351dd63c564238346d8e95baf22
mina-gwak <mina.gwak@kakao.com>
2024-10-29 00:22:58 +0900

    Feat: PD-112 비밀번호 리셋 요청 API 연결

--- src/pages/password/ui/find-password-page/index.tsx
+++ src/pages/password/ui/find-password-page/index.tsx
@@ -1,21 +1,47 @@
 'use client'
 
 import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
 
+import {
+  EmailVerificationCodeExceptionCode,
+  HttpError,
+  ResourceNotFoundExceptionCode,
+} from 'shared/api'
 import { Button, Label, Layout } from 'shared/ui'
 
 import { Form } from 'features/form'
 
-import { defaultValues } from '../../model/form-values'
+import { useRequestPasswordReset } from '../../api/use-request-password-reset'
+import { defaultValues, FormValues } from '../../model/form-values'
 import * as rules from '../../model/rules'
 
 import * as css from './variants'
 
 export const FindPasswordPage = () => {
-  const form = useForm({
+  const form = useForm<FormValues>({
     defaultValues,
     reValidateMode: 'onSubmit',
   })
+
+  const requestPasswordReset = useRequestPasswordReset()
+
+  const handleRequestPasswordResetError = (error: HttpError) => {
+    if (error.code === EmailVerificationCodeExceptionCode.TOO_MANY_REQUEST) {
+      toast.error(
+        'You have requested too many times. Please try again in 10 minutes.',
+      )
+    } else if (error.code === ResourceNotFoundExceptionCode.USER_NOT_FOUND) {
+      toast.error("We couldn't find an account with that email")
+    }
+  }
+
+  const submitForm = (data: FormValues) => {
+    requestPasswordReset.mutate(data, {
+      onSuccess: () => {}, // TODO: 논의 필요
+      onError: handleRequestPasswordResetError,
+    })
+  }
 
   return (
     <Layout className={css.layout()}>
@@ -28,7 +54,7 @@ export const FindPasswordPage = () => {
             <Form.ErrorMessage />
           </Form.Control>
         </div>
-        <Button size="large" fullWidth>
+        <Button size="large" fullWidth onClick={form.handleSubmit(submitForm)}>
           Send password reset link
         </Button>
       </Form>
