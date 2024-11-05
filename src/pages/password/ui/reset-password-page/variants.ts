40f70e10330610eb281f5fdd29c4c00df794a70e
mina-gwak <mina.gwak@kakao.com>
2024-11-05 10:25:02 +0900

    Fix: 페이지 타이틀 font-weight 값 변경

--- src/pages/password/ui/reset-password-page/variants.ts
+++ src/pages/password/ui/reset-password-page/variants.ts
@@ -1,5 +1,5 @@
 import { cva } from 'class-variance-authority'
 
 export const layout = cva('flex flex-col items-center gap-10')
-export const heading = cva('display-small text-gray-900')
+export const heading = cva('display-small font-bold text-gray-900')
 export const form = cva('flex flex-col gap-10')
--- src/shared/ui/error/error-page.tsx
+++ src/shared/ui/error/error-page.tsx
@@ -10,7 +10,9 @@ export const ErrorPage = ({ title, content }: Props) => {
   return (
     <Layout className="flex flex-col items-center justify-center gap-4">
       <Icon name="ExclamationMark" color={colors.error} size="xxLarge" />
-      <h1 className="display-small text-center text-gray-900">{title}</h1>
+      <h1 className="display-small text-center font-bold text-gray-900">
+        {title}
+      </h1>
       {content && (
         <div className="title-medium whitespace-pre-line text-center text-gray-900">
           {content}
