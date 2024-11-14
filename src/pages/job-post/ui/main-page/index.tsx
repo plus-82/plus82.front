7003c37c7cac819a8b4e7331942fd52e956b26a0
mina-gwak <mina.gwak@kakao.com>
2024-11-14 20:01:24 +0900

    Feat: PD-125 메인 페이지 마크업

--- src/pages/job-post/ui/main-page/index.tsx
+++ src/pages/job-post/ui/main-page/index.tsx
@@ -1,3 +1,84 @@
+'use client'
+
+import { Chip, Filter, Layout } from 'shared/ui'
+
+import { useJobPostFilter } from '../../lib/use-job-post-filter'
+import { Card } from '../card'
+import { ResetButton } from '../reset-button'
+
 export const MainPage = () => {
-  return <div>Main Page</div>
+  const {
+    filters,
+    isFilterExist,
+    updateLocationFilter,
+    updateStudentTypeFilter,
+    removeLocationFilter,
+    removeStudentTypeFilter,
+    resetFilters,
+  } = useJobPostFilter()
+
+  return (
+    <Layout wide>
+      <div className="mb-10 flex h-[430px] w-full items-center justify-center rounded-3xl bg-gray-200">
+        Banner
+      </div>
+      <section className="mb-20">
+        <h2 className="display-small mb-6 text-gray-900">Closing soon</h2>
+        <div className="flex flex-wrap gap-x-5 gap-y-8">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <Card key={index} />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h2 className="display-small mb-4 text-gray-900">Job posting</h2>
+        <div className="mb-6 flex flex-col gap-4">
+          <div className="flex gap-2">
+            <Filter
+              name="location"
+              value={filters.locations}
+              onChange={updateLocationFilter}
+            >
+              <Filter.Item value="seoul">Seoul</Filter.Item>
+              <Filter.Item value="gyeonggi-do">Gyeonggi-do</Filter.Item>
+            </Filter>
+            <Filter
+              name="studentType"
+              value={filters.studentTypes}
+              onChange={updateStudentTypeFilter}
+            >
+              <Filter.Item value="adult">Adult</Filter.Item>
+              <Filter.Item value="element">ElementSchool</Filter.Item>
+            </Filter>
+            {isFilterExist && <ResetButton onClick={resetFilters} />}
+          </div>
+          {isFilterExist && (
+            <div className="flex gap-2">
+              {filters.locations.map(location => (
+                <Chip key={location} selected>
+                  <Chip.Label>{location}</Chip.Label>
+                  <Chip.RemoveButton
+                    onClick={() => removeLocationFilter(location)}
+                  />
+                </Chip>
+              ))}
+              {filters.studentTypes.map(item => (
+                <Chip key={item} selected>
+                  <Chip.Label>{item}</Chip.Label>
+                  <Chip.RemoveButton
+                    onClick={() => removeStudentTypeFilter(item)}
+                  />
+                </Chip>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="flex flex-wrap gap-x-5 gap-y-8">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Card key={index} />
+          ))}
+        </div>
+      </section>
+    </Layout>
+  )
 }
