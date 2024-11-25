57eb176990d9b18be2b178e48d6ada2f20ee992a
mina-gwak <mina.gwak@kakao.com>
2024-11-26 00:19:54 +0900

    Feat: PD-129 debounce 훅 추가

---
+++ src/shared/lib/debounce.ts
@@ -0,0 +1,35 @@
+'use client'
+
+import { useCallback, useRef } from 'react'
+
+export const useDebounce = <T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number,
+) => {
+  const timeoutRef = useRef<NodeJS.Timeout>()
+
+  const debouncedCallback = useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        callback(...args)
+      }, delay)
+    },
+    [callback, delay],
+  )
+
+  const immediateCallback = useCallback(
+    (...args: Parameters<T>) => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current)
+      }
+      callback(...args)
+    },
+    [callback],
+  )
+
+  return { debouncedCallback, immediateCallback }
+}
