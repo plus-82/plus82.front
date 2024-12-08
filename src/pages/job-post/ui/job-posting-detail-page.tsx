fb99f99af5de49bb14ef8d1223d8802c1903c1cb
mina-gwak <mina.gwak@kakao.com>
2024-12-09 00:57:06 +0900

    Feat: PD-133 JobPostingDetail 페이지 마크업

--- src/pages/job-post/ui/job-posting-detail-page.tsx
+++ src/pages/job-post/ui/job-posting-detail-page.tsx
@@ -1,3 +1,49 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import { Suspense } from 'react'
+
+import { Button, Layout } from 'shared/ui'
+
+import {
+  convertToJobPost,
+  PostingDetail,
+  PostingTitle,
+} from 'entities/job-post'
+
+import { useJobPost } from '../api/use-job-post'
+
+type Params = {
+  jobPostId: string
+}
+
+const JobPostingDetail = () => {
+  const params = useParams<Params>()
+
+  const { data } = useJobPost(Number(params?.jobPostId))
+
+  const jobPost = convertToJobPost(data)
+
+  return (
+    <Layout wide className="flex gap-5">
+      <div className="flex-grow">
+        <div className="mb-6 h-[410px] w-full rounded-2xl bg-gray-200" />
+        <PostingDetail jobPost={data} />
+      </div>
+      <div>
+        <div className="sticky top-6 flex w-[340px] flex-col gap-6 rounded-2xl border border-gray-200 bg-white p-6">
+          <PostingTitle jobPost={jobPost} size="medium" />
+          <Button>Apply Now</Button>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
 export const JobPostingDetailPage = () => {
-  return <div>JobPostingDetailPage</div>
+  return (
+    <Suspense fallback={<div>Loading</div>}>
+      <JobPostingDetail />
+    </Suspense>
+  )
 }
