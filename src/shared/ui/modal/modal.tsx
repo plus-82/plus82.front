6ed1359edb100d7ae61fb3679b7c17efa864f915
mina-gwak <mina.gwak@kakao.com>
2025-01-06 23:37:46 +0900

    Fix: PD-134 모달 컴포넌트를 shadcn에서 제공하는 것으로 변경

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,37 +1,130 @@
 'use client'
 
-import { useRouter } from 'next/router'
-import { ReactNode } from 'react'
+import * as DialogPrimitive from '@radix-ui/react-dialog'
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  forwardRef,
+  HTMLAttributes,
+} from 'react'
 
 import { colors } from 'shared/config'
 import { cn } from 'shared/lib'
 
 import { Icon } from '../icon'
 
-type Props = {
-  className?: string
-  children: ReactNode
-}
+const ModalRoot = DialogPrimitive.Root
 
-export const Modal = ({ children, className }: Props) => {
-  const router = useRouter()
+const ModalTrigger = DialogPrimitive.Trigger
 
-  const handleClose = () => {
-    router.back()
-  }
+const ModalPortal = DialogPrimitive.Portal
 
-  return (
-    <div className="absolute flex h-full w-screen items-center justify-center bg-[rgba(0,0,0,0.5)]">
-      <div className="flex min-w-80 flex-col rounded-lg border border-gray-300 bg-white p-6 first-line:min-h-32">
-        <button
-          onClick={handleClose}
-          aria-label="Close modal"
-          className="mb-3 ml-auto block w-fit"
-        >
-          <Icon name="Close" color={colors.gray[900]} />
-        </button>
-        <div className={cn('flex-grow', className)}>{children}</div>
-      </div>
-    </div>
-  )
-}
+const ModalClose = DialogPrimitive.Close
+
+const ModalOverlay = forwardRef<
+  ElementRef<typeof DialogPrimitive.Overlay>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Overlay>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Overlay
+    ref={ref}
+    className={cn(
+      'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-black/50',
+      className,
+    )}
+    {...props}
+  />
+))
+ModalOverlay.displayName = DialogPrimitive.Overlay.displayName
+
+const ModalContent = forwardRef<
+  ElementRef<typeof DialogPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <ModalPortal>
+    <ModalOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn(
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-3 border bg-white p-6 shadow-lg duration-200 sm:rounded-lg',
+        className,
+      )}
+      {...props}
+    >
+      <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground ml-auto w-fit rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
+        <Icon name="Close" color={colors.gray[900]} />
+        <span className="sr-only">Close</span>
+      </DialogPrimitive.Close>
+      {children}
+    </DialogPrimitive.Content>
+  </ModalPortal>
+))
+ModalContent.displayName = DialogPrimitive.Content.displayName
+
+const ModalHeader = ({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col space-y-1.5 text-center sm:text-left',
+      className,
+    )}
+    {...props}
+  />
+)
+ModalHeader.displayName = 'ModalHeader'
+
+const ModalFooter = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => (
+  <div
+    className={cn(
+      'flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2',
+      className,
+    )}
+    {...props}
+  />
+)
+ModalFooter.displayName = 'ModalFooter'
+
+const ModalTitle = forwardRef<
+  ElementRef<typeof DialogPrimitive.Title>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Title>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Title
+    ref={ref}
+    className={cn(
+      'text-lg font-semibold leading-none tracking-tight',
+      className,
+    )}
+    {...props}
+  />
+))
+ModalTitle.displayName = DialogPrimitive.Title.displayName
+
+const ModalDescription = forwardRef<
+  ElementRef<typeof DialogPrimitive.Description>,
+  ComponentPropsWithoutRef<typeof DialogPrimitive.Description>
+>(({ className, ...props }, ref) => (
+  <DialogPrimitive.Description
+    ref={ref}
+    className={cn('text-muted-foreground text-sm', className)}
+    {...props}
+  />
+))
+ModalDescription.displayName = DialogPrimitive.Description.displayName
+
+const Modal = Object.assign(ModalRoot, {
+  Trigger: ModalTrigger,
+  Portal: ModalPortal,
+  Overlay: ModalOverlay,
+  Close: ModalClose,
+  Content: ModalContent,
+  Header: ModalHeader,
+  Footer: ModalFooter,
+  Title: ModalTitle,
+  Description: ModalDescription,
+})
+
+export { Modal }
