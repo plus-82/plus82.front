92d6c4a2f39af77881c982343c99f4b68fe006d6
mina-gwak <mina.gwak@kakao.com>
2025-01-09 23:38:28 +0900

    Feat: PD-134 이력서 API, 카드 컴포넌트 추가

---
+++ src/entities/resume/api/get-resumes.ts
@@ -0,0 +1,23 @@
+import { apiClient } from 'shared/api'
+import type { Pagination } from 'shared/api'
+import { getCookie } from 'shared/lib'
+
+import { Resume } from '../model/resume'
+
+type GetResumesResponse = Pagination<Resume>
+
+export const getResumes = async () => {
+  const accessToken = await getCookie('accessToken')
+
+  const response = await apiClient.get<GetResumesResponse>({
+    endpoint: '/resumes/me',
+    queryParams: {
+      rowCount: 100,
+    },
+    option: {
+      authorization: `Bearer ${accessToken}`,
+    },
+  })
+
+  return response
+}
---
+++ src/entities/resume/index.ts
@@ -0,0 +1,3 @@
+export { resumeQueries } from './api/query'
+export type { Resume } from './model/resume'
+export { Card } from './ui/card'
