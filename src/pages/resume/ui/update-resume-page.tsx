1eb3fe1c040fd5a575f73ba0c81819e56574102e
mina-gwak <mina.gwak@kakao.com>
2025-02-25 00:45:39 +0900

    Fix: PD-168 파일 이력서인 경우 상세 페이지에서 파일 보여주도록 수정

--- src/pages/resume/ui/update-resume-page.tsx
+++ src/pages/resume/ui/update-resume-page.tsx
@@ -1,13 +1,29 @@
 import { getResume } from 'entities/resume'
 import { updateResume } from 'entities/resume/api/update-resume'
 import { ResumeForm } from 'features/resume-form'
+import { PDFViewer } from 'features/show-resume-file'
+import { Layout } from 'shared/ui'
+
+type Params = {
+  resumeId: string
+}
 
 export const UpdateResumePage = async ({
   params,
 }: {
-  params: { resumeId: string }
+  params: Promise<Params>
 }) => {
-  const resume = await getResume(params.resumeId)
+  const { resumeId } = await params
+
+  const resume = await getResume(resumeId)
+
+  if (resume.filePath) {
+    return (
+      <Layout wide>
+        <PDFViewer filePath={resume.filePath} />
+      </Layout>
+    )
+  }
 
   return <ResumeForm resume={resume} submit={updateResume} />
 }
