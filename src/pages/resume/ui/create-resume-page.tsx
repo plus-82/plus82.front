fd0cea13fb3305d622f9ab30e94e0b0cba394672
mina-gwak <mina.gwak@kakao.com>
2025-03-23 00:36:06 +0900

    Fix: PD-193 이력서 생성 시 유저 이미지를 기본으로 보여줌

--- src/pages/resume/ui/create-resume-page.tsx
+++ src/pages/resume/ui/create-resume-page.tsx
@@ -1,6 +1,14 @@
 import { createResume } from 'entities/resume'
+import { getUserMe } from 'entities/user'
 import { ResumeForm } from 'features/resume-form'
 
-export const CreateResumePage = () => {
-  return <ResumeForm submit={createResume} />
+export const CreateResumePage = async () => {
+  const user = await getUserMe()
+
+  return (
+    <ResumeForm
+      userProfileImage={user.profileImagePath}
+      submit={createResume}
+    />
+  )
 }
