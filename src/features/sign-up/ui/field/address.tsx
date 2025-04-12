63b05ac3af659357245e0b9583e4a0afdd877a32
mina-gwak <mina.gwak@kakao.com>
2025-04-12 21:57:34 +0900

    Feat: PD-248 주소 검색 API 적용

--- src/features/sign-up/ui/field/address.tsx
+++ src/features/sign-up/ui/field/address.tsx
@@ -1,22 +1,67 @@
 import { useTranslations } from 'next-intl'
+import { ChangeEvent, useState } from 'react'
+import DaumPostcode, { type Address as AddressType } from 'react-daum-postcode'
+import { useFormContext } from 'react-hook-form'
 
 import { fieldCss } from 'shared/form'
-import { Button, Label, TextField } from 'shared/ui'
+import { Button, Label, Modal, TextField } from 'shared/ui'
+
+import { convertToLocationType } from '../../lib/util'
 
 export const Address = () => {
   const t = useTranslations()
+
+  const [isOpen, setIsOpen] = useState(false)
+  const [address, setAddress] = useState('')
+  const [detailedAddress, setDetailedAddress] = useState('')
+
+  const form = useFormContext()
+
+  const handleButtonClick = () => {
+    setIsOpen(true)
+  }
+
+  const handleCompleteSearchingCode = (data: AddressType) => {
+    form.setValue('detailedAddress', data.address)
+    form.setValue('locationType', convertToLocationType(data.sido))
+
+    setAddress(data.address)
+    setDetailedAddress('')
+    setIsOpen(false)
+  }
+
+  const handleAddressChange = (event: ChangeEvent<HTMLInputElement>) => {
+    form.setValue('detailedAddress', `${address} ${event.target.value}`)
+    setDetailedAddress(event.target.value)
+  }
 
   return (
     <div className={fieldCss.fieldWrapper()}>
       <Label required>{t('field.address.label')}</Label>
       <div className="flex gap-2">
-        <TextField readOnly className="bg-gray-100" />
-        <Button variant="lined" size="large" className="w-[120px] shrink-0">
-          주소 검색
-        </Button>
+        <TextField value={address} readOnly className="bg-gray-100" />
+        <Modal open={isOpen} onOpenChange={setIsOpen}>
+          <Button
+            variant="lined"
+            size="large"
+            className="w-[120px] shrink-0"
+            onClick={handleButtonClick}
+          >
+            주소 검색
+          </Button>
+          <Modal.Content className="h-[600px] w-[450px]">
+            <DaumPostcode
+              style={{ height: '500px' }}
+              onComplete={handleCompleteSearchingCode}
+            />
+          </Modal.Content>
+        </Modal>
       </div>
-      <TextField readOnly className="bg-gray-100" />
-      <TextField placeholder={t('field.address.placeholder')} />
+      <TextField
+        value={detailedAddress}
+        placeholder={t('field.address.placeholder')}
+        onChange={handleAddressChange}
+      />
     </div>
   )
 }
