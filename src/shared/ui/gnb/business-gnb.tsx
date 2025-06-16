6dc4123e1ad105b1ab4898d9f7c1d373c3a76b5f
mina-gwak <mina.gwak@kakao.com>
2025-06-17 00:42:14 +0900

    Fix: 학원용 GNB 업데이트

--- src/shared/ui/gnb/business-gnb.tsx
+++ src/shared/ui/gnb/business-gnb.tsx
@@ -33,51 +33,48 @@ export const BusinessGNB = () => {
         <div className={cn(css.innerWrapper())}>
           <div className={cn(css.leftSection())}>
             <Logo onClick={handleLogoClick} />
-            {isDev && (
-              <Navigation.Root>
-                {/* <Navigation.Item value="/business/find-teacher">
+            <Navigation.Root>
+              {/* <Navigation.Item value="/business/find-teacher">
                   {t('tab.find-teacher')}
                 </Navigation.Item> */}
-                <Navigation.Item value="/business/applicant-management">
-                  {t('tab.applicant-management')}
-                </Navigation.Item>
-                <Navigation.Item value="/business/academy-detail">
-                  {t('tab.academy-detail')}
-                </Navigation.Item>
-                <Navigation.Item value="/business/job-posting">
-                  {t('tab.job-posting')}
-                </Navigation.Item>
-              </Navigation.Root>
-            )}
+              <Navigation.Item value="/business/applicant-management">
+                {t('tab.applicant-management')}
+              </Navigation.Item>
+              <Navigation.Item value="/business/academy-detail">
+                {t('tab.academy-detail')}
+              </Navigation.Item>
+              <Navigation.Item value="/business/job-posting">
+                {t('tab.job-posting')}
+              </Navigation.Item>
+            </Navigation.Root>
           </div>
           <div className={cn(css.rightSection())}>
-            {isDev &&
-              (notAuthenticated ? (
-                <div className={cn(css.textButtons())}>
-                  <Button
-                    as="a"
-                    href="/business/sign-in"
-                    variant="text"
-                    size="small"
-                  >
-                    {t('button.sign-in')}
-                  </Button>
-                  <div className={cn(css.divider())} />
-                  <Button
-                    as="a"
-                    href="/business/sign-up"
-                    variant="text"
-                    size="small"
-                  >
-                    {t('button.sign-up')}
-                  </Button>
-                </div>
-              ) : (
-                <div className="mr-6 flex items-center gap-4">
-                  <NotificationButton />
-                  <BusinessButton />
-                </div>
-              ))}
+            {notAuthenticated ? (
+              <div className={cn(css.textButtons())}>
+                <Button
+                  as="a"
+                  href="/business/sign-in"
+                  variant="text"
+                  size="small"
+                >
+                  {t('button.sign-in')}
+                </Button>
+                <div className={cn(css.divider())} />
+                <Button
+                  as="a"
+                  href="/business/sign-up"
+                  variant="text"
+                  size="small"
+                >
+                  {t('button.sign-up')}
+                </Button>
+              </div>
+            ) : (
+              <div className="mr-6 flex items-center gap-4">
+                <NotificationButton />
+                <BusinessButton />
+              </div>
+            )}
             <Button as="a" href="/" variant="lined" size="small">
               {t('button.go-to-teacher-home')}
             </Button>
