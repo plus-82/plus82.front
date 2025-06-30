0b849ef604db6e4452a3227889f55518ebf41c9a
mina-gwak <mina.gwak@kakao.com>
2025-07-01 00:19:25 +0900

    Feat: GoogleTagManager 추가

--- app/(business)/business/layout.tsx
+++ app/(business)/business/layout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { NextIntlClientProvider } from 'next-intl'
 import { getLocale } from 'next-intl/server'
@@ -24,6 +25,11 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
       <body className={cn('font-pretendard', 'flex min-h-dvh w-full flex-col')}>
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
+        {process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER ? (
+          <GoogleTagManager
+            gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER}
+          />
         ) : null}
         <NextIntlClientProvider>
           <AppProviders basePath="/business/api/auth">
